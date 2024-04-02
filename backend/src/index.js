1d7694e482fee216c08980d864c82c67eb982ba4
Isaac Chan <isaacchancollege@gmail.com>
2024-04-01 20:48:56 -0700

    initial db connection established

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,36 @@
+import { PrismaClient } from "@prisma/client";
+import express from "express";
 
-const express = require("express");
 const app = express();
 const port = 4000;
+
+const prisma = new PrismaClient();
+
 
 app.get('/', (req, res) => {
     res.send('Hello World')
 })
+
+async function main() {
+  
+    const allUsers = await prisma.user.findMany({
+      include: {
+        posts: true,
+        profile: true,
+      },
+    })
+    console.dir(allUsers, { depth: null })
+}
+  
+
+main()
+    .then( async () => {
+        await prisma.$disconnect()
+    })
+    .catch( async (e) => {
+        console.error(e)
+        await prisma.$disconnect()
+        process.exit(1)
+    })
 
 app.listen(port);
