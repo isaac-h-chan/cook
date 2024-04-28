1f7ce60ea86ec5b833762524fe791f084af7c0d5
Isaac Chan <isaacchancollege@gmail.com>
2024-04-28 15:28:44 -0700

    basic authentication

--- backend/src/auth/auth.js
+++ backend/src/auth/auth.js
@@ -2,8 +2,10 @@ import express from "express"
 import cookieParser from "cookie-parser"
 import prisma from "../db/db_client.js"
 import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library"
+import bcrypt from "bcrypt"
 
 const auth_router = express.Router()
+
 
 // middlewares
 auth_router.use(express.json())
@@ -18,11 +20,14 @@ auth_router.get("/", (req, res) => {
 auth_router.post("/signup", async (req, res) => {
 
 	try {
+
+		const hashed_password = await bcrypt.hash(req.body.password, 10)
+
 		const user = await prisma.user.create({
 			data: {
 				name: req.body.name,
 				email: req.body.email,
-				password: req.body.password,
+				password: hashed_password
 			}
 		})
 
@@ -40,6 +45,38 @@ auth_router.post("/signup", async (req, res) => {
 		}
 	}
 
+})
+
+
+/*
+ * Request Body Params
+ *	- email
+ *	- password
+ */
+auth_router.post("/authenticate_user", async (req, res) => {
+
+	try {
+
+		const user = await prisma.user.findUniqueOrThrow({
+			where: {
+				email: req.body.email
+			}
+		})
+		if (await bcrypt.compare(req.body.password, user.password)) {
+			res.status(200).send("Successfully Authenticated")
+		} else {
+			res.status(401).send("Authentication Failure")
+		}
+
+	} catch(e) {
+
+		if (e instanceof PrismaClientKnownRequestError) {
+			if (e.code === "P2025") {
+				res.status(404).send("User with email " + req.body.email + " not found")
+			}
+		}
+
+	}
 })
 
 
